d567e81e4a1c3f7680d0befaca7fcd6886ad6bb1
Fraser Hess <fraser@sweeterrhythm.com>
2009-04-16 20:37:50 -0600

    Add ERROR: to database errors

--- PHP/dbconnect.php
+++ PHP/dbconnect.php
@@ -9,8 +9,8 @@ $dbpassword = '';
 
 // Setup connection
 
-$dbconnection = mysql_pconnect($dbhost,$dbuser,$dbpassword) or die("Unable to connect to database");
-mysql_select_db($dbname,$dbconnection) or die(mysql_error($dbconnection));
+$dbconnection = mysql_pconnect($dbhost,$dbuser,$dbpassword) or die("ERROR: Unable to connect to database");
+mysql_select_db($dbname,$dbconnection) or die("ERROR: " . mysql_error($dbconnection));
 
 
 ?>
