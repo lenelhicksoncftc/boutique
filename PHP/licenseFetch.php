45ea919d53bf2fed74a076c22db9fdedfb2d85a6
Fraser Hess <fraser@sweeterrhythm.com>
2009-04-25 07:40:20 -0600

    Add licenseFetch.php

---
+++ PHP/licenseFetch.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!isset($_POST['transactionID'])) {
+	exit("ERROR: Missing parameter in POST request");
+}
+
+require 'dbconnect.php';
+require 'transactionFunctions.php';
+
+$transactionID = $_POST['transactionID'];
+
+$license = licenseForTransaction($transactionID);
+
+if ($license === FALSE) {
+	exit("ERROR: Transaction ID could not be found");
+} else {
+	echo $license;
+}
+
+?>
