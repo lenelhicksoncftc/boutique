b989af686cf8a206224aa7f70b8dd9d2d0838d4f
Fraser Hess <fraser@sweeterrhythm.com>
2009-05-04 21:45:07 -0600

    Change default page title to 'Cocoa Boutique'

--- PHP/admin/header.inc.php
+++ PHP/admin/header.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($page_title)==FALSE) $page_title="store";
+if (isset($page_title) === FALSE) $page_title="Cocoa Boutique";
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
