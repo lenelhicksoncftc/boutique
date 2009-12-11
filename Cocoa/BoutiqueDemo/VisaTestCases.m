48f78f7493f6fe4743b2d4d20676558a4c4028ac
Fraser Hess <fraser@sweeterrhythm.com>
2009-12-11 13:15:11 -0700

    Add more Visa tests

--- Cocoa/BoutiqueDemo/VisaTestCases.m
+++ Cocoa/BoutiqueDemo/VisaTestCases.m
@@ -20,13 +20,18 @@
 	STAssertTrue([cb isValidVisaNumber:@"4012888888881881"],@"Card number 4012888888881881 should return YES");
 	// Test 13 character number
 	STAssertTrue([cb isValidVisaNumber:@"4222222222222"],@"Card number 4222222222222 should return YES");
-	
+
 	// Invalid card numbers that look like Visa numbers
 	STAssertFalse([cb isValidVisaNumber:nil], @"Card number nil should return NO");
 	STAssertFalse([cb isValidVisaNumber:@"4408041234567890"],@"Card number 4408041234567890 should return NO");
 	STAssertFalse([cb isValidVisaNumber:@"41111111111114"],@"Card number 41111111111114 should return NO");
 	STAssertFalse([cb isValidVisaNumber:@"4111111111111110"],@"Card number 4111111111111110 should return NO");
-	
+	STAssertFalse([cb isValidVisaNumber:@"4452517017358008"],@"Card number 4452517017358008 should return NO");
+	STAssertFalse([cb isValidVisaNumber:@"4640180219717067"],@"Card number 4640180219710767 should return NO");
+	STAssertFalse([cb isValidVisaNumber:@"0"],@"Card number 0 should return NO");
+	STAssertFalse([cb isValidVisaNumber:@"4"],@"Card number 4 should return NO");
+	STAssertFalse([cb isValidVisaNumber:@"422222222222"],@"Card number 422222222222 should return NO");
+
 	// Test other cards
 	STAssertFalse([cb isValidVisaNumber:@"5500000000000004"],@"Card number 5500000000000004 should return NO");
 	STAssertFalse([cb isValidVisaNumber:@"6491000990139423"],@"Card number 6441000990139424 should return NO");
