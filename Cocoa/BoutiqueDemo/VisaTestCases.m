d6d5b254f9b33b966bd965bb8626f23613664090
Fraser Hess <fraser@sweeterrhythm.com>
2009-12-14 12:07:59 -0700

    Refactor Visa tests

--- Cocoa/BoutiqueDemo/VisaTestCases.m
+++ Cocoa/BoutiqueDemo/VisaTestCases.m
@@ -14,27 +14,21 @@
 
 - (void)testVisaNumbers {
 	CocoaBoutique *cb = [[CocoaBoutique alloc] init];
+	
 	// Test valid Visa card numbers
-	STAssertTrue([cb isValidVisaNumber:@"4408041234567893"],@"Card number 4408041234567893 should return YES");
-	STAssertTrue([cb isValidVisaNumber:@"4111111111111111"],@"Card number 4111111111111111 should return YES");
-	STAssertTrue([cb isValidVisaNumber:@"4012888888881881"],@"Card number 4012888888881881 should return YES");
-	// Test 13 character number
-	STAssertTrue([cb isValidVisaNumber:@"4222222222222"],@"Card number 4222222222222 should return YES");
-
-	// Invalid card numbers that look like Visa numbers
+	NSArray *goodNumbers = [NSArray arrayWithObjects:@"4408041234567893", @"4111111111111111", @"4012888888881881", @"4222222222222",nil];
+	
+	for (NSString *num in goodNumbers) {
+		STAssertTrue([cb isValidVisaNumber:num], @"Card number %@ should test as valid", num);
+	}
+	
+	// Invalid card numbers
 	STAssertFalse([cb isValidVisaNumber:nil], @"Card number nil should return NO");
-	STAssertFalse([cb isValidVisaNumber:@"4408041234567890"],@"Card number 4408041234567890 should return NO");
-	STAssertFalse([cb isValidVisaNumber:@"41111111111114"],@"Card number 41111111111114 should return NO");
-	STAssertFalse([cb isValidVisaNumber:@"4111111111111110"],@"Card number 4111111111111110 should return NO");
-	STAssertFalse([cb isValidVisaNumber:@"4452517017358008"],@"Card number 4452517017358008 should return NO");
-	STAssertFalse([cb isValidVisaNumber:@"4640180219717067"],@"Card number 4640180219710767 should return NO");
-	STAssertFalse([cb isValidVisaNumber:@"0"],@"Card number 0 should return NO");
-	STAssertFalse([cb isValidVisaNumber:@"4"],@"Card number 4 should return NO");
-	STAssertFalse([cb isValidVisaNumber:@"422222222222"],@"Card number 422222222222 should return NO");
-
-	// Test other cards
-	STAssertFalse([cb isValidVisaNumber:@"5500000000000004"],@"Card number 5500000000000004 should return NO");
-	STAssertFalse([cb isValidVisaNumber:@"6491000990139423"],@"Card number 6441000990139424 should return NO");
+	NSArray *badNumbers = [NSArray arrayWithObjects:@"4408041234567890", @"41111111111114", @"4111111111111110", @"4452517017358008", @"4640180219717067", @"0", @"4", @"422222222222", @"5500000000000004", @"6491000990139423", nil];
+	
+	for (NSString *num in badNumbers) {
+		STAssertFalse([cb isValidVisaNumber:num], @"Card number %@ should test as invalid", num);
+	}
 
 	[cb release];
 }
