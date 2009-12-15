f8d1b0fd44ca9bd824f80986c61587f27bb6d131
Fraser Hess <fraser@sweeterrhythm.com>
2009-12-14 19:58:44 -0700

    Refactor Discover tests

--- Cocoa/BoutiqueDemo/DiscoverTestCases.m
+++ Cocoa/BoutiqueDemo/DiscoverTestCases.m
@@ -14,20 +14,22 @@
 
 - (void)testDiscoverNumbers {
 	CocoaBoutique *cb = [[CocoaBoutique alloc] init];
-	// Test valid Discover card numbers
-	STAssertTrue([cb isValidDiscoverNumber:@"6011111111111117"],@"Card number 6011111111111117 should return YES");
-	STAssertTrue([cb isValidDiscoverNumber:@"6011000990139424"],@"Card number 6011000990139424 should return YES");
-	STAssertTrue([cb isValidDiscoverNumber:@"6441000990139424"],@"Card number 6441000990139424 should return YES");
-	STAssertTrue([cb isValidDiscoverNumber:@"6491000990139423"],@"Card number 6491000990139423 should return YES");
 	
-	// Invalid card numbers that look like Discover numbers
+	// Test valid Discover card numbers
+	NSArray *goodNumbers = [NSArray arrayWithObjects:@"6011111111111117", @"6011000990139424", @"6441000990139424", @"6491000990139423",nil];
+	
+	for (NSString *num in goodNumbers) {
+		STAssertTrue([cb isValidDiscoverNumber:num], @"Card number %@ should test as valid", num);
+	}
+	
+	// Invalid card numbers
 	STAssertFalse([cb isValidDiscoverNumber:nil], @"Card number nil should return NO");
-	STAssertFalse([cb isValidDiscoverNumber:@"601100099013"],@"Card number 601100099013 should return NO");
-	STAssertFalse([cb isValidDiscoverNumber:@"6491000990139422"],@"Card number 6491000990139422 should return NO");
-
-	// Test other cards
-	STAssertFalse([cb isValidDiscoverNumber:@"4111111111111111"],@"Card number 4111111111111111 should return NO");
-	STAssertFalse([cb isValidDiscoverNumber:@"5424000000000015"],@"Card number 5424000000000015 should return NO");
+	
+	NSArray *badNumbers = [NSArray arrayWithObjects:@"601100099013", @"6491000990139422", @"4111111111111111", @"5424000000000015",nil];
+	
+	for (NSString *num in badNumbers) {
+		STAssertFalse([cb isValidDiscoverNumber:num], @"Card number %@ should test as invalid", num);
+	}
 
 	[cb release];
 }
